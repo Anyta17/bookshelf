749e683b0c1d3b879e1ac176286a8eee5c287528
Anna Blahitka <anettaabl@gmail.com>
2023-06-27 18:42:34 +0300

    I made this project because I love to read

--- shelves/admin.py
+++ shelves/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from shelves.models import Publication, Genre, Author, Book
+
+admin.site.register(Publication)
+admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(Book)
--- shelves/models.py
+++ shelves/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Publication(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    genre = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.genre
+
+
+class Author(AbstractUser):
+
+    class Meta:
+        verbose_name = "author"
+        verbose_name_plural = "authors"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    pub_house = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author, related_name="authors")
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title}: {self.genre}"
