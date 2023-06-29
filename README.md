4ddea2eb870a012369c0da0cda8fd68be92fad68
Anna Blahitka <anettaabl@gmail.com>
2023-06-29 14:51:30 +0300

    Add bookshelfdata.json

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-"# bookshelf" 
+# Bookshelf Project
+
+Django project for managing books, authors, genres and publications in Bookshelf
+
+## Installation
+
+Python3 must be already installed
+
+```shell
+git clone https://github.com/Anyta17/bookshelf.git
+cd bookshelf
+python -m venv venv
+venv\Scripts\activate
+pip install -r requirements.txt
+puthon manage.py runserver # starts Django Server
+```
+
+## Features
+
+* Authentication functionality for Author/User
+* Managing books authors genres & publications directly from website interface
+* Powerful admin panel for advanced managing
+
+## 
+- Use the following command to load prepared data from fixture to test and debug your code:
+  
+`python manage.py loaddata bookshelfdata.json`
+
+- You can use the following superuser (or create another yourself):
+    - Login: testuser
+    - Password: testpassword
--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9bwy@g=&wuzlpsb#f77z^a41)=fo)03m(1!fv@!km4@$bntxzo"
+load_dotenv()
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-9bwy@g=&wuzlpsb#f77z^a41)=fo)03m(1!fv@!km4@$bntxzo")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
