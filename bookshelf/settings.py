3f0d219ef846adcb2bab900e2d38a41f7f0f3620
Anna Blahitka <anettaabl@gmail.com>
2023-07-10 17:07:02 +0300

    Edit settings.py

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
@@ -94,6 +95,10 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# dj-database-url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 
 # Password validation
